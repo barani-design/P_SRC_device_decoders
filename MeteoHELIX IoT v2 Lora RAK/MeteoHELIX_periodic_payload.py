dc56003026536a42155778edac3b0bb4b595b481
wolf2sk <klcopavol@gmail.com>
2024-11-06 13:23:53 +0100

    2024-11-06-1323

---
+++ MeteoHELIX IoT v2 Lora RAK/MeteoHELIX_periodic_payload.py
@@ -0,0 +1,3 @@
+## Ahoj ja som skrypt. Ked mi das na vstupl platny payload, vyplujem ti dekodovane data
+## data budu pekne v poradi ako su v exceli, float na 4 desatinne,
+## zarovnane tabelatormi..
