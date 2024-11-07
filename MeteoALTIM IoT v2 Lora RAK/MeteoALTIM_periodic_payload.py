0d23f3bf2edf8070fab8296182e980aaacc48589
SimonKrajcik <simon.krajcik@baranidesign.com>
2024-11-07 14:05:18 +0100

    upd Altim parser

--- MeteoALTIM IoT v2 Lora RAK/MeteoALTIM_periodic_payload.py
+++ MeteoALTIM IoT v2 Lora RAK/MeteoALTIM_periodic_payload.py
@@ -1,33 +1,36 @@
-## Ahoj ja som skrypt. Ked mi das na vstupl platny payload, vyplujem ti dekodovane data
-## data budu pekne v poradi ako su v exceli, float na 4 desatinne,
-## zarovnane tabelatormi..              #Seva kamo, bardzo fajny skrypt sa nakodzi
+#Meteo Altim parser ver.***
+import sys
 
 class parser:
-    base = 16                                      #hexa base
+    base = 16                                                                                                           #hexa base
 
-    indexStart = 0                                 #starting position in payload
-    indexLen   = 8                                 #length of variable in bits
+    indexStart = 0                                                                                                      #starting position in payload
+    indexLen   = 8                                                                                                      #length of variable in bits
 
     battStart  = 8
     battLen    = 1
+    battRes    = 0.2
+    battOffset = 3.3
 
     tempStart  = 9
     tempLen    = 9
-    tempRes    = 0.25                              # if resolution is different than 1
-    tempOffset = -50                               # temp offset because this is minimal value
+    tempRes    = 0.25                                                                                                   # if resolution is different than 1
+    tempOffset = -50                                                                                                    # temp offset because this is minimal value
 
     humStart   = 18
     humLen     = 7
 
-    pressBaseStart = 25                            # starting position for pressures
-    pressNum   = 6                                 # numbers of pressure sensor's
+    pressBaseStart = 25                                                                                                 # starting position for pressures
+    pressNum   = 6                                                                                                      # numbers of pressure sensor's
     pressLen   = 17
     pressOffset = 30000
 
     dbgStart   = 127
     dbgLen     = 1
 
-    def __init__(self, inputString, numOfBytes):
+    def __init__(self, inputString, numOfBytes):                                                                        # internal storage for parsed variables
+        self.battState = None
+        self.batt = None
         self.dbg = None
         self.press = [0] * self.pressNum
         self.hum = None
@@ -37,26 +40,24 @@ class parser:
         self.inputString = inputString
         self.numOfBytes = numOfBytes
 
-    def getBinString(self):
+    def getBinString(self):                                                                                             # convert HEX string to binary string
         self.binString = bin(int(self.inputString, self.base))[2:].zfill(self.numOfBytes*8)
         return self.binString
 
-    def getBinStringList(self):
+    def getBinStringList(self):                                                                                         # convert HEX string to binary list
         self.binStringList = list(bin(int(self.inputString, self.base))[2:].zfill(self.numOfBytes*8))
         return self.binStringList
 
-    def parseOneVariable(self, varStartPos, varLen, varOffset):
+    def parseOneVariable(self, varStartPos, varLen, varOffset):                                                         # parse only one variable, you has to setup start position of first bit and variable length
         self.binStringList = list(bin(int(self.inputString, self.base))[2:].zfill(self.numOfBytes * 8))
         num = ''.join(self.binStringList[varStartPos:varStartPos+varLen])
         print("Parser variable", int(num, 2) + varOffset)
 
-    def parsePayload(self, enablePrint):                          #if print 1 then print parsed payload
-        self.binStringList = list(bin(int(self.inputString, self.base))[2:].zfill(self.numOfBytes * 8))
-
+    def parsePayload(self, enablePrint):                                                                                # if print 1 then print parsed payload
+        self.binStringList = list(bin(int(self.inputString, self.base))[2:].zfill(self.numOfBytes * 8))                 # parse payload string and convert it to variables
         self.index = int(''.join(self.binStringList[self.indexStart : self.indexStart + self.indexLen]),2)
-
-        # BATERKAAAA ESTE
-
+        self.battState = int(''.join(self.binStringList[self.battStart : self.battStart + self.battLen]),2)
+        self.batt = ((self.index % 5) * self.battRes) + self.battOffset
         self.temp = (int(''.join(self.binStringList[self.tempStart: self.tempStart + self.tempLen]), 2) * self.tempRes) + self.tempOffset
         self.hum = int(''.join(self.binStringList[self.humStart : self.humStart + self.humLen]),2)
         self.dbg = int(''.join(self.binStringList[self.dbgStart: self.dbgStart + self.dbgLen]), 2)
@@ -66,16 +67,30 @@ class parser:
 
         if enablePrint == 1:
             print("Index: " + str(self.index))
-            print("Batt: NONE")
+
+            if self.battState == 1:
+                print("Batt: >" + str(format(self.batt, '.4f')) + "V")
+            else:
+                print("Batt: <" + str(format(self.batt, '.4f')) + "V")
+
             print("Temp: " + str(self.temp) + "C")
             print("Hum: " + str(self.hum) + "%")
 
             for i in range(self.pressNum):
-                print("Press_"+ str(i) +" "+ str(self.press[i]) + "Pa")
+                print("Press_"+ str(i) +": "+ str(self.press[i]) + "Pa")
 
-            print("DBG:", self.dbg)
+            if self.dbg == 1:
+                print("DBG: Pressure sensor error!", self.dbg)
+            else:
+                print("DBG: No error")
 
+##### EXAMPLE CODE #####
 
+# print("MeteoAltim parser example code")                                                                               # uncomment if you want to run it from IDE
+# d = parser("your payload to decode",16)
+# d.parsePayload(1)
 
-d = parser("06CB52448C62443123D8922C48EA161A",16)
-d.parsePayload(1)
+if __name__ == "__main__":                                                                                              # uncomment if you want to run it from CMD line
+    print("MeteoAltim parser example code")
+    d = parser(str(sys.argv[1]),int(sys.argv[2]))
+    d.parsePayload(1)
