b9b2cf58ca89395d378ca7d61461491e65e50903
Pavol Klco <klcopavol@gmail.com>
2025-02-22 15:19:34 +0100

    upd

--- MeteoHELIX IoT PRO Gen2/MeteoHelix_IoT_Pro_Gen2_FW1.02.00x.js
+++ MeteoHELIX IoT PRO Gen2/MeteoHelix_IoT_Pro_Gen2_FW1.02.00x.js
@@ -48,13 +48,22 @@ function decodeUplink(input) {
         return Math.round(number * factor) / factor;
     }
     
-    function batteryIndicator(index, battery_bit, min_value=3.3) {
-        var remainder = index % 10;
-        var result = remainder < 4 ? remainder * 0.2 + min_value : remainder * 0.2 + min_value - 1;
+   function batteryIndicator(index, battery_bit, min_value=3.3) {
+        var remainder = index % 5;
         
-        var rounded = Math.round(result * 10) / 10;
-        return battery_bit === 1 ? `> ${rounded} V` : `< ${rounded} V`;
-    }    
+        if ( remainder > 4)
+        {
+          return 0;
+        }
+        
+        else
+        {
+          var result = remainder < 4 ? remainder * 0.2 + min_value : remainder * 0.2 + min_value - 1;
+          var rounded = Math.round(result * 10) / 10;
+          return battery_bit === 1 ? `> ${rounded} V` : `< ${rounded} V`;
+        }
+        
+    }  
 
     bindata = data2bits(bytes);
 
