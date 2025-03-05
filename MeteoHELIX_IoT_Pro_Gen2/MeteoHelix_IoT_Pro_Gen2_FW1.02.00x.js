f11bcfd52148acd3e530c59b19d2dfc43e0a72b0
Pavol Klco <klcopavol@gmail.com>
2025-03-05 10:30:11 +0100

    Update MeteoHelix_IoT_Pro_Gen2_FW1.02.00x.js

--- MeteoHELIX_IoT_Pro_Gen2/MeteoHelix_IoT_Pro_Gen2_FW1.02.00x.js
+++ MeteoHELIX_IoT_Pro_Gen2/MeteoHelix_IoT_Pro_Gen2_FW1.02.00x.js
@@ -73,8 +73,9 @@ function decodeUplink(input) {
     var battery = batteryIndicator(index, battery_bit);
 
     var temp_avg = precisionRound(bitShift(14)*0.01, 2);
-    temp_avg = temp_avg + (-50);
-    tem_avg = Math.round(temp_avg * 10) / 10;
+    temp_avg = precisionRound(temp_avg + (-50), 2);
+    
+    //tem_avg = Math.round(temp_avg * 10) / 10;
 
     var temp_min_diff = precisionRound(bitShift(8)*0.05, 2);
     var temp_max_diff = precisionRound(bitShift(8)*0.05, 2);
@@ -82,8 +83,9 @@ function decodeUplink(input) {
     var temp_min = temp_avg - temp_min_diff
     var temp_max = temp_avg + temp_min_diff
     
-    temp_min = Math.round(temp_min * 10) / 10;
-    temp_max = Math.round(temp_max * 10) / 10;
+    //tem_avg = Math.round(temp_avg * 10) / 10;
+    temp_min = Math.round(temp_min * 100) / 100;
+    temp_max = Math.round(temp_max * 100) / 100;
     
     var humidity = precisionRound(bitShift(9)*0.2, 2);
     var pressure = precisionRound(bitShift(15)*2.5, 2);
